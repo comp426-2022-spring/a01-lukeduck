0b657965f1a85ac525e1953c92305376c058b2ef
lukeduck <lukeduck@live.unc.edu>
2022-01-25 17:00:54 -0500

    remove comment

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-/*
-const http = require('http')
-
-const port = process.env.PORT || 5000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello, World!</h1>')
-})
-
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`)
-})
-*/
-
 // Require http module
 const http = require('http')
 // Require fs module
